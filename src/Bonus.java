6b4ff6cbb3601abca40a0ab583177028492726ba
Lenovo <Lenovo@LAPTOP-RBRLDFQV>
2022-02-18 21:18:14 +0100

    Ajouter des attributes

--- src/Bonus.java
+++ src/Bonus.java
@@ -1,5 +1,9 @@
 
 public class Bonus {
+	private int idBonus;
+	private String nom;
+	private int diametre;
+	private Position positionBonus;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
