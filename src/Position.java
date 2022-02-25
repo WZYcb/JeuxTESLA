39dbe0be241eec584edfe3de8f6f8ebecc0860d6
Lenovo <Lenovo@172.30.147.189>
2022-02-25 18:28:50 +0100

    modification

--- src/Position.java
+++ src/Position.java
@@ -3,10 +3,35 @@ public class Position {
 	private int x;
 	private int y;
 	
-	public void position(int x,int y) {
+	
+	public void position(int x,int y){
+		this.x=x;
+		this.y=y;
 		
 	}
-    
+	public int getx()
+	{
+		return x;
+	}
 	
+    public int gety()
+	{
+		return y;
+	}
+    
+    public int gauche() {
+    	x=x-1;
+    }
+    
+    public int droite() {
+    	x=x+1;
+    }
 
+    public int haut() {
+    	y=y+1;
+    }
+	
+    public int bas() {
+    	y=y-1;
+    }
 }
--- src/Vitesse.java
+++ src/Vitesse.java
@@ -4,14 +4,49 @@ public class Vitesse {
 	private int y;
 	
     public void vitesse(int x,int y) {
-		
+    	this.x=x;
+    	this.y=y;
 	}
     
-    public void directionAngle() {
+    public int directionAngle() {
+    	Random random= new Random();
+    	int num=random.nextInt(4);
+    	
+        
+    	switch(num) {
+    	   case 0:
+    	       return gauche();
+    	   case 1:
+    		   return droite();
+    	   case 2:
+    		   return haut();
+    	   case 3:
+    		   return bas();   	   
+    	}		
+    	}
 		
-	}
+    public int gauche() {
+    	x=x-1;
+    }
+    
+    public int droite() {
+    	x=x+1;
+    }
+
+    public int haut() {
+    	y=y+1;
+    }
 	
+    public int bas() {
+    	y=y-1;
+    }
+	
+    
+    
+    
 	public void amplitude() {
+		if()
+		
 		
 	}
 	
