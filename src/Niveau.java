1fc71130afac1839fcb18bb5a8b6e1a0b4793398
Lenovo <Lenovo@172.30.147.189>
2022-03-11 14:28:29 +0100

    modification

--- src/Niveau.java
+++ src/Niveau.java
@@ -13,8 +13,79 @@ public class Niveau {
 	
 	public Niveau(int idNiveau,String nom,int nbAsterides,int nbObjetsGravitationnels,int nbRecharges,
 			int nbBonus,Position pointDepart,Position pointArrivee,Position tailleNiveau,int duree) {
+		this.idNiveau=idNiveau;
+		this.nom=nom;
+		this.nbAsterides=nbAsterides;
+		this.nbObjetsGravitationnels=nbObjetsGravitationnels;
+		this.nbRecharges=nbRecharges;
+		this.nbBonus=nbBonus;
+		this.pointDepart=pointDepart;
+		this.pointArrivee=pointArrivee;
+		this.tailleNiveau=tailleNiveau;
+		this.duree=duree;
 		
 	}
+	public int getidNiveau() {
+		return idNiveau;
+	}
+    public String getnom() {
+    	return nom;
+    }
+    public int getnbAsterides() {
+    	return nbAsterides;
+    }
+    public int getnbObjetsGravitationnels() {
+    	return nbObjetsGravitationnels;
+    }
+    public int getnbRecharges() {
+    	return nbRecharges;
+    
+    }
+    public int getnbBonus() {
+    	return nbBonus;
+    }
+    public Position getpointDepart() {
+    	return pointDepart;
+    }
+    public Position getpointArrivee() {
+    	return pointArrivee;
+    }
+    public Position gettailleNiveau() {
+    	return tailleNiveau;
+    }
+    public int getduree() {
+    	return duree;
+    }
+    public void setidNiveau(int idNiveau) {
+    	this.idNiveau=idNiveau;
+    }
+    public void setnom(String nom) {
+    	this.nom=nom;
+    }
+    public void setnbAsterides(int nbAsterides) {
+    	this.nbAsterides=nbAsterides;
+    }
+    public void setnbObjetsGravitationnels(int nbObjetsGravitationnels) {
+    	this.nbObjetsGravitationnels=nbObjetsGravitationnels;
+    }
+    public void setnbRecharges(int nbRecharges) {
+    	this.nbRecharges=nbRecharges;
+    }
+    public void setnbBonus(int nbBonus) {
+    	this.nbBonus=nbBonus;
+    }
+    public void setpointDepart(Position pointDepart) {
+    	this.pointDepart=pointDepart;
+    }
+    public void pointArrivee(Position pointArrivee) {
+    	this.pointArrivee=pointArrivee;
+    }
+    public void settailleNiveau(Position tailleNiveau) {
+    	this.tailleNiveau=tailleNiveau;
+    }
+    public void setduree(int duree) {
+    	this.duree=duree;
+    }
 	
     public void placerAsteroides() {
     	
--- src/ObjetGravitationnel.java
+++ src/ObjetGravitationnel.java
@@ -1,14 +1,66 @@
 
+
+
+
 public class ObjetGravitationnel {
-	private int idObjetGravitationnel;
-	private String nom;
-	private int diametre;
-	private int diaminfluence;
-	private Position positionObjetGravitationnel;
-	private float coeffGravite;
-	
-	public ObjetGravitationnel(int idObjetGravitationnel,String nom,int diametre,int diaminfluence,
-			Position positionObjetGravitationnel,float coeffGravite) {
+		//variable
+		private int idObjetGravitationnel;
+		private String nom;
+		private int diametre;
+		private int diaminfluence;
+		private Position positionObjetGravitationnel;
+		private float coeffGravite;
 		
+		//constructeur
+		public ObjetGravitationnel(int idObjetGravitationnel,String nom,int diametre,int diaminfluence,
+				Position positionObjetGravitationnel,float coeffGravite) {
+			   this.idObjetGravitationnel=idObjetGravitationnel;
+			   this.coeffGravite=coeffGravite;
+			   this.diametre=diametre;
+			   this.diaminfluence=diaminfluence;
+			   this.nom=nom;
+			   this.positionObjetGravitationnel=positionObjetGravitationnel;
+			   
+		}
+		public int getidObjetGravitationnel() {
+			return idObjetGravitationnel;
+		}
+		
+	    public String getnom() {
+	    	return nom;
+	    }
+	    
+	    public int getdiametre() {
+	    	return diametre;
+	    	
+	    }
+	    public int getdiaminfluence() {
+	    	return diaminfluence;
+	    }
+	    public Position getpositionObjetGravitationnel() {
+	    	return positionObjetGravitationnel;
+	    }
+	    public float getcoeffGravite() {
+	    	return coeffGravite;
+	    }
+	    public void setidObjetGravitationnel(int idObjetGravitationnel) {
+	    	this.idObjetGravitationnel=idObjetGravitationnel;
+	    	
+	    }
+	    public void setnom(String nom) {
+	    	this.nom=nom;
+	    }
+	    public void setdiametre(int diametre) {
+	    	this.diametre=diametre;
+	    }
+	    public void setdiaminfluence(int diaminfluence) {
+	    	this.diaminfluence=diaminfluence;
+	    }
+	    public void setpositionObjetGravitationnel(Position positionObjetGravitationnel) {
+	        this.positionObjetGravitationnel=positionObjetGravitationnel;
 	}
-}
+	    public void setcoeffGravite(float coeffGravite) {
+	    	this.coeffGravite=coeffGravite;
+	    }
+	 
+	}
